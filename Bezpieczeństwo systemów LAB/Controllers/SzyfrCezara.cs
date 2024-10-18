d0096fba124577bb86e47acda841cbc5660d762b
Vlkhizhniak <vladyslavkhyzhniak@gmail.com>
2024-10-18 13:06:49 +0200

    Controller and FormView

---
+++ Bezpieczeństwo systemów LAB/Controllers/SzyfrCezara.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Bezpieczeństwo_systemów_LAB.Controllers;
+
+public class SzyfrCezara : Controller
+{
+    public IActionResult Form()
+    {
+        return View();
+    }
+}
