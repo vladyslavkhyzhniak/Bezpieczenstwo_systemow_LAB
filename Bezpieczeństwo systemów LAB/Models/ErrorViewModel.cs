0b8be78e201dd97d370529ef893166ee59acdf4a
Vlkhizhniak <vladyslavkhyzhniak@gmail.com>
2024-10-18 12:13:16 +0200

    init

---
+++ Bezpieczeństwo systemów LAB/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Bezpieczeństwo_systemów_LAB.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
