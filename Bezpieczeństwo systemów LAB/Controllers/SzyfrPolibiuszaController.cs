72ff77aa0cf4466432872490176c7d6962742090
VladyslavKhyzhniak <Vladyslav.khyzhniak@microsoft.wsei.edu.pl>
2024-10-20 23:23:02 +0200

    Szyfr Polibiusza FormView
    Signed-off-by: VladyslavKhyzhniak <Vladyslav.khyzhniak@microsoft.wsei.edu.pl>

---
+++ Bezpieczeństwo systemów LAB/Controllers/SzyfrPolibiuszaController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Bezpieczeństwo_systemów_LAB.Controllers;
+
+public class SzyfrPolibiuszaController : Controller
+{
+    
+    public IActionResult Form()
+    {
+        return View();
+    }
+}
