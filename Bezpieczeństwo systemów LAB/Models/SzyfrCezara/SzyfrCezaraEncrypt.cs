aa0eb9ffab879dcddb278a6d4550bcc1b5f0a789
VladyslavKhyzhniak <Vladyslav.khyzhniak@microsoft.wsei.edu.pl>
2024-10-20 17:07:25 +0200

    Encrypt method
    Signed-off-by: VladyslavKhyzhniak <Vladyslav.khyzhniak@microsoft.wsei.edu.pl>

---
+++ Bezpieczeństwo systemów LAB/Models/SzyfrCezara/SzyfrCezaraEncrypt.cs
@@ -0,0 +1,38 @@
+﻿namespace Bezpieczeństwo_systemów_LAB.Models;
+
+public class SzyfrCezaraEncrypt
+{
+    public string? text { get; set; }
+    public int? step { get; set; }
+
+    public bool IsValid()
+    {
+        return text != null && step != null;
+    }
+
+    public string Encrypt()
+    {
+        if (step<0)
+        {
+            step += 35;
+        }
+        string result = null;
+
+        string Clearedtext = string.Join("", text.Split(' ')).ToLower();
+
+        for (int i = 0; i < Clearedtext.Length; i++)
+        {
+            char letter = Clearedtext[i];
+            int index = Array.IndexOf(Enum.GetNames(typeof(AlphabetEnum)), letter.ToString());
+            int EncryptedIndex =index +step.Value;
+            if (EncryptedIndex > 34)
+            {
+                EncryptedIndex -=35;
+            }
+            
+            AlphabetEnum? EncryptedLetter =(AlphabetEnum)EncryptedIndex;
+            result+=EncryptedLetter.ToString();
+        }
+        return result;
+    }
+}
